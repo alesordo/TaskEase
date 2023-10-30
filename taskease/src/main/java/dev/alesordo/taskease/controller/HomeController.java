b77c338cd23838ae80bd677315fecfe9d9cd38aa
alesordo <ale.sordo@hotmail.it>
2023-10-30 13:24:06 +0100

    Created Spring Boot project - added OAuth2 authentication

---
+++ taskease/src/main/java/dev/alesordo/taskease/controller/HomeController.java
@@ -0,0 +1,17 @@
+package dev.alesordo.taskease.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello website!";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello secured!";
+    }
+}
