83be9d5d3a719a42ef19a2e0e97789678a55fa96
alesordo <85616887+alesordo@users.noreply.github.com>
2023-11-04 10:44:56 +0100

    Developed basic frontend skeleton - to be completed

--- taskease-frontend/src/app/app-routing.module.ts
+++ taskease-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProjectViewComponent} from "./pages/project-view/project-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProjectViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- taskease-frontend/src/app/app.module.ts
+++ taskease-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectViewComponent } from './pages/project-view/project-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectViewComponent
   ],
   imports: [
     BrowserModule,
